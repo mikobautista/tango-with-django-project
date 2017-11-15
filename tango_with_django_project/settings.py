b83e05a75de6703e3a993a23bcb1fa9add25487f
Miko Bautista <michaeljoseph.bautista@gmail.com>
2017-11-15 19:19:40 +0000

    auth with django-registration-redux

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
+    'registration',  # add in the registration package
 )
 
 MIDDLEWARE_CLASSES = (
@@ -105,4 +106,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media direct
 # 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 # )
 
-LOGIN_URL = '/rango/login/'
+REGISTRATION_OPEN = True        # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                #  and are trying to access pages requiring authentication
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,15 +1,23 @@
-from django.conf import settings # New Import
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static # New Import
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from registration.backends.simple.views import RegistrationView
+
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+
+    # Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 if settings.DEBUG:
